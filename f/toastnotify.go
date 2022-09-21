2e1ec85e6b3b3cbadb9b4b5f6cf21c82dd4e65e7
Jannek Rodenburger <jannek.rodenburger@vrg.de>
2022-09-21 10:58:39 +0200

    integrated notification toast

---
+++ f/toastnotify.go
@@ -0,0 +1,26 @@
+package f
+
+import (
+	"github.com/jnnkrdb/jlog"
+	"gopkg.in/toast.v1"
+)
+
+// creates a small default notification
+//
+// Parameters:
+//   - `appid` : string > application id, strongly recommended
+//   - `title` : string > title of the notification, strongly recommended
+//   - `message` : string > further message like informations and so on
+func Notify(appid, title, message string) {
+
+	notification := toast.Notification{
+		AppID:   appid,
+		Title:   title,
+		Message: message,
+	}
+
+	if err := notification.Push(); err != nil {
+
+		jlog.PrintObject(notification, err)
+	}
+}
--- go.mod
+++ go.mod
@@ -2,4 +2,10 @@ module github.com/jnnkrdb/cordb
 
 go 1.19
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	github.com/jnnkrdb/jlog v1.0.2
+	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
