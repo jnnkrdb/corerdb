9f4283b68a7dbb397d7c70161920b3532affeaaa
Jannek Rodenburger <jannek.rodenburger@vrg.de>
2022-09-22 13:38:07 +0200

    changed toast notify

--- f/toastnotify.go
+++ f/toastnotify.go
@@ -1,8 +1,13 @@
 package f
 
+//	gopkg.in/toast.v1 v1.0.0-20180812000517-0a84660828b2
+// require github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
+
+/*
+
 import (
 	"github.com/jnnkrdb/jlog"
-//	"gopkg.in/toast.v1"
+	"gopkg.in/toast.v1"
 )
 
 // creates a small default notification
@@ -11,7 +16,6 @@ import (
 //   - `appid` : string > application id, strongly recommended
 //   - `title` : string > title of the notification, strongly recommended
 //   - `message` : string > further message like informations and so on
-/*
 func Notify(appid, title, message string) {
 
 	notification := toast.Notification{
