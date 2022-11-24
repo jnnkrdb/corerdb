a44e25ec2f3c97f85652deb8171d05077fb86b76
Jannek Rodenburger <jannek.rodenburger@vrg.de>
2022-11-24 10:06:49 +0100

    integrated mailng functionality

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/jnnkrdb/corerdb
 
 go 1.19
 
-require gopkg.in/yaml.v2 v2.4.0
+require (
+	gopkg.in/mail.v2 v2.3.1
+	gopkg.in/yaml.v2 v2.4.0
+)
+
+require gopkg.in/alexcesaro/quotedprintable.v3 v3.0.0-20150716171945-2caba252f4dc // indirect
---
+++ mail/dialer.go
@@ -0,0 +1,67 @@
+package mail
+
+import (
+	"crypto/tls"
+	"strings"
+
+	"github.com/jnnkrdb/corerdb/fnc"
+	"github.com/jnnkrdb/corerdb/prtcl"
+	gomail "gopkg.in/mail.v2"
+)
+
+// sending the mailmessage
+func SendMail(mailserver SMTPServer, msg MailMessage) (err error) {
+
+	prtcl.Log.Println("starting mailflow")
+
+	mail := gomail.NewMessage()
+
+	mail.SetHeader("From", msg.From)
+
+	mail.SetHeader("To", strings.Join(msg.To, ";"))
+
+	if len(msg.Cc) > 0 {
+		mail.SetHeader("Cc", strings.Join(msg.Cc, ";"))
+	}
+
+	if len(msg.Bcc) > 0 {
+		mail.SetHeader("Bcc", strings.Join(msg.Bcc, ";"))
+	}
+
+	mail.SetHeader("Subject", msg.Subject)
+
+	if fnc.StringInList(msg.TextFormat, []string{
+		"text/html",
+		"text/plain",
+		"text/richtext",
+		"text/rtf",
+		"text/xml",
+	}) {
+
+		mail.SetBody(msg.TextFormat, msg.Body)
+
+	} else {
+
+		mail.SetBody("text/plain", msg.Body)
+	}
+
+	// --- placeholder attachments
+
+	// mail.Attach()
+
+	dialer := gomail.NewDialer(
+		mailserver.Address,
+		mailserver.Port,
+		mailserver.Username,
+		fnc.UnencodeB64(mailserver.Password),
+	)
+
+	dialer.TLSConfig = &tls.Config{InsecureSkipVerify: true}
+
+	if err = dialer.DialAndSend(mail); err != nil {
+
+		prtcl.Log.Println("error sending mailmessage:", err)
+	}
+
+	return
+}
